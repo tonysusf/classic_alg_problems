d8d3cd7f215d8ec7af200a6397fa1cf3924dac00
Tony Su <makefire@gmail.com>
2023-03-10 19:54:37 -0800

    2 sum

--- py/two_sum/two_sum.py
+++ py/two_sum/two_sum.py
@@ -16,3 +16,12 @@ def two_sum(nums, target):
 nums = [2, 7, 11, 15]
 target = 9
 assert(two_sum(nums, target) == [0, 1])
+
+nums = [3, 2, 4]
+target = 6
+assert(two_sum(nums, target) == [1, 2])
+
+nums = [3, 3]
+target = 6
+assert(two_sum(nums, target) == [0, 1])
+
