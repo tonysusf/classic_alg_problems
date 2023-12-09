6d6df0e5fd4bc0397f2a42c86404357cf2a85654
Tony Su <tonysuglobal@gmail.com>
2023-12-08 16:30:51 -0800

    Python basics

---
+++ py/python_basics/python_basics.py
@@ -0,0 +1,19 @@
+
+##################
+# String
+##################
+# Reverse a string
+a = 'hello world'
+a2 = a[::-1]
+
+# Last char of a string
+s = 'helloworld'
+s[-1]
+
+##################
+# List
+##################
+# Last item of a list
+l = [1, 2, 3, 4]
+l[-1]
+
