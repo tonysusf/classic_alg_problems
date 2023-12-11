0a69e2041631b56528559cf3ed44844125be1658
Tony Su <tonysuglobal@gmail.com>
2023-12-11 14:40:12 -0800

    Subarray sum to k

---
+++ py/subarray_sum_to_k/subarr_sum_to_k.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/subarray-sum-equals-k/
+
+from collections import defaultdict
+
+def subarray_sum(nums, k):
+    print(nums, k)
+    count = 0
+    run_sum = 0
+    lookup = defaultdict(int)
+    lookup[0] = 1 # for checking itself
+    for i in range(len(nums)):
+        run_sum += nums[i]
+        if (run_sum - k) in lookup:
+            count += lookup[run_sum - k]
+        lookup[run_sum] += 1
+    print(count)
+    return count
+
+
+assert subarray_sum([1,1,1], 2) == 2
+
+assert subarray_sum([1,2,3], 3) == 2
+
+assert subarray_sum([1,-1,1], 0) == 2
