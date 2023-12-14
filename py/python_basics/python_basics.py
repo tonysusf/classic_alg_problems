a96f07a2b7fab19fb80ff24ab74c0f4ea27dee46
Tony Su <tonysuglobal@gmail.com>
2023-12-14 15:48:34 -0800

    basics

--- py/python_basics/python_basics.py
+++ py/python_basics/python_basics.py
@@ -22,6 +22,7 @@ l.reverse()
 
 # Reverse a list
 l[::-1]
+list(reversed(l))
 
 # Sort a list in-place
 l.sort()
@@ -30,4 +31,25 @@ l.sort(reverse=True)
 # Sort a list
 sorted(l)
 sorted(l, reverse=True)
+
+# Integer division
+7//3
+
+# Count a item in the list
+l.count(2)
+
+# Count every item in a list
+from collections import Counter
+l = [1, 2, 3, 4, 2, 3, 3]
+c = Counter(l)
+print(dict(c))
+
+# Double linked list (with fast appends and pops on either end)
+from collections import deque
+d = deque([1, 2, 3])
+d.append(4)
+d.pop()
+d.popleft()
+d.appendleft()
+
 
