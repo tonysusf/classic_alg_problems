d10a95335d6f563e710f96ed1ac23e6d91f627df
Tony Su <tonysuglobal@gmail.com>
2023-12-15 11:41:50 -0800

    cleanup

--- py/longest_nonrepeat_substr/longest_non_repeat_substring.py
+++ py/longest_nonrepeat_substr/longest_non_repeat_substring.py
@@ -2,16 +2,15 @@
 
 def longest_non_repeat_substring_len(s: str):
     print('s is', s)
-    l = r = 0 # left and right pointer for sliding window
+    l = 0 # left pointer
     lookup = {} # dict for char to index
     max_len = 0
     for i in range(len(s)):
-        k = s[i]
-        r += 1
-        if k in lookup and lookup[k] >= l:
-            l = lookup[k] + 1
-        lookup[k] = i
-        max_len = max(max_len, r - l)
+        c = s[i]
+        if c in lookup and lookup[c] >= l:
+            l = lookup[c] + 1 # point l to be after the duplicate, +1 so no duplicate
+        lookup[c] = i # save the current char with index
+        max_len = max(max_len, i - l + 1)
     return max_len
 
 
