c3e443249be2621877ce3e9a1b3724b90ff45ba4
Tony Su <tonysuglobal@gmail.com>
2023-12-14 16:28:29 -0800

    test cases

--- py/subarray_sum_to_k/subarr_sum_to_k.py
+++ py/subarray_sum_to_k/subarr_sum_to_k.py
@@ -17,8 +17,14 @@ def subarray_sum(nums, k):
     return count
 
 
-assert subarray_sum([1,1,1], 2) == 2
+assert subarray_sum([1, 1, 1], 2) == 2
 
-assert subarray_sum([1,2,3], 3) == 2
+assert subarray_sum([1, 2, 3], 3) == 2
 
-assert subarray_sum([1,-1,1], 0) == 2
+assert subarray_sum([1, -1, 1], 0) == 2
+
+assert subarray_sum([1], 2) == 0
+
+assert subarray_sum([1], 1) == 1
+
+assert subarray_sum([1, -1, -1], -2) == 1
