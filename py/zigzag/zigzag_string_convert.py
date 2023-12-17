483fca789ccfa6421b6f81c9c2a83602c60aadad
Tony Su <tonysuglobal@gmail.com>
2023-12-17 14:00:08 -0800

    Adjust ZigZag

--- py/zigzag/zigzag_string_convert.py
+++ py/zigzag/zigzag_string_convert.py
@@ -11,20 +11,24 @@ import math
 def zigzag_convert(input_str, num_of_rows):
     if num_of_rows == 1:
         return input_str
-    z_len = num_of_rows * 2 - 2 # num of chars of each zigzag
-    n = math.ceil(len(input_str) / z_len) # num of zigzags
-    result = [[] for k in range(num_of_rows)] # init
-    for z_idx in range(n):
-        for row in range(num_of_rows):
-            idx = z_idx * z_len + row # index in the input string
-            if(idx < len(input_str)):
-                result[row].append(input_str[idx])
-            idx2 = (z_idx+1) * z_len - row
-            if row > 0 and row < num_of_rows - 1 and idx2 < len(input_str):
-                result[row].append(input_str[idx2])
-    return ''.join([''.join(row) for row in result])
+    zz_len = num_of_rows * 2 - 2 # num of chars of each zigzag
+    num_of_zz = math.ceil(len(input_str) / zz_len) # num of zigzags
+    results = []
+    for row in range(num_of_rows): # row by row scan
+        l = []
+        for zz_idx in range(num_of_zz): # scan each zigzag for the row
+            idx1 = zz_idx * zz_len + row
+            if(idx1 < len(input_str)):
+                l.append(input_str[idx1])
+            idx2 = (zz_idx+1) * zz_len - row 
+            if 0 < row < num_of_rows - 1 and idx2 < len(input_str):
+                l.append(input_str[idx2])
+        tmp_line = ''.join(l)
+        results.append(tmp_line)
+    return ''.join(results)
 
 assert zigzag_convert('PAYPALISHIRING', 3) == 'PAHNAPLSIIGYIR'
 assert zigzag_convert('PAYPALISHIRING', 1) == 'PAYPALISHIRING'
 assert zigzag_convert('A', 1) == 'A'
+assert zigzag_convert('AB', 2) == 'AB'
 assert zigzag_convert('PAYPALISHIRING', 4) == 'PINALSIGYAHRPI'
