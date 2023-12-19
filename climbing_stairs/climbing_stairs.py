0b11b59fc1c068c51070a36c98d577f8a91e1094
Tony Su <tonysuglobal@gmail.com>
2023-12-18 20:44:45 -0800

    Climbing Stairs using Fibonacci

---
+++ climbing_stairs/climbing_stairs.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/climbing-stairs/solution/
+
+
+def climb_stairs(n):
+    if n == 2:
+        return 2
+    if n == 1:
+        return 1
+    a = 1
+    b = 2
+    c = 0
+    for i in range(n-2): # fibonacci 
+        c = a + b
+        a, b = b, c
+    return c
+
+assert climb_stairs(1) == 1
+assert climb_stairs(2) == 2
+assert climb_stairs(3) == 3
+assert climb_stairs(4) == 5
