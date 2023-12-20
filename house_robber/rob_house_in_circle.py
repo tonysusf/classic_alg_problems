1327608b0da718745e0d42879b8efd45d3be0701
Tony Su <tonysuglobal@gmail.com>
2023-12-20 08:47:59 -0800

    Rob house in a circle

---
+++ house_robber/rob_house_in_circle.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/house-robber-ii/submissions/
+
+def rob_cicrle(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    a = _rob(nums[:-1]) # no last one
+    b = _rob(nums[1:]) # no 1st one
+    return max(a, b)
+
+def _rob(nums):
+    t1 = t2 = 0
+    for current in nums:
+        t1, t2 = max(current + t2, t1), t1
+    return t1
+
+nums = [2,3,2]
+assert rob_cicrle(nums) == 3
+
+
+nums = [1,2,3,1]
+assert rob_cicrle(nums) == 4
+
+
+nums = [1,2,3]
+assert rob_cicrle(nums) == 3
