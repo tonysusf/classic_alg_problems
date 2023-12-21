29e3e89c6ad23b187f8dcd5eb4472c8ff3e52cfd
Tony Su <tonysuglobal@gmail.com>
2023-12-21 14:30:01 -0800

    cleanup

--- subarray_sum/max_subarray_sum.py
+++ subarray_sum/max_subarray_sum.py
@@ -2,10 +2,10 @@
 
 
 def max_sub_array_sum(nums):
-    current_sum = max_sum = nums[0]
-    for num in nums[1:]:
-        current_sum = max(num, current_sum + num)
-        max_sum = max(max_sum, current_sum)
+    current = max_sum = nums[0]
+    for i in range(1, len(nums)):
+        current = max(nums[i], current + nums[i])
+        max_sum = max(max_sum, current)
     return max_sum
 
 
@@ -14,7 +14,6 @@ assert max_sub_array_sum(nums) == 6
 
 nums = [1]
 assert max_sub_array_sum(nums) == 1
-
 
 nums = [5,4,-1,7,8]
 assert max_sub_array_sum(nums) == 23
