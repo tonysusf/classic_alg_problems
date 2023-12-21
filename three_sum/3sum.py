8b40c908106fcaf0384e7c8a9fffeded782e1571
Tony Su <tonysuglobal@gmail.com>
2023-12-21 12:10:38 -0800

    cleanup

--- three_sum/3sum.py
+++ three_sum/3sum.py
@@ -1,39 +1,37 @@
 # https://leetcode.com/problems/3sum/
+# given an array find all triplets sum to 0, no duplicates
 
-
-def three_sum(nums):
+def three_sum(nums): # runtime O(n^2), space O(n)
     results = []
-    nums.sort()
-    print('------input', nums)
-    i = 0
-    while i < len(nums) and nums[i] <= 0: # up to the zero only
-        if i == 0 or nums[i - 1] != nums[i]: # skip duplicate
-            _find_two_sum(nums, i, results)
-        i += 1
+    nums.sort() # sort it 1st!
+    print('nums', nums)
+    for idx1 in range(len(nums)):
+        if nums[idx1] > 0: # scan up to zero
+            break
+        if idx1 > 0 and nums[idx1 - 1] == nums[idx1]: # skip duplicate
+            continue
+
+        # find the other 2 nums after current index summing to 0
+        lookup = set()
+        for idx2 in range(idx1+1, len(nums)): # scan the right part of idx
+            target = 0 - nums[idx1] - nums[idx2]
+            if target in lookup:
+                results.append([nums[idx1], nums[idx2], target]) # found it
+                while idx2 < len(nums) - 1 and nums[idx2] == nums[idx2 + 1]: # skip duplicate
+                    idx2 += 1
+            else:
+                lookup.add(nums[idx2])
     print('results', results)
     return results
 
-def _find_two_sum(nums, idx, results): # for a given index, find the other 2 nums sum to 0
-    lookup = set()
-    p = idx + 1
-    while p < len(nums): # scan the right part of idx
-        target = 0 - nums[idx] - nums[p]
-        if target in lookup:
-            results.append([nums[idx], nums[p], target])
-            while p < len(nums) - 1 and nums[p] == nums[p + 1]: # skip duplicate
-                p += 1
-        lookup.add(nums[p])
-        p += 1
 
 def _to_str(l):
+    tmp_l = []
     for row in l:
         row.sort()
-        for i in range(len(row)):
-            row[i] = str(row[i])
-    l = [','.join(k) for k in l]
-    l.sort()
-    out = '|'.join(l)
-    return out
+        tmp_l.append(','.join([str(k) for k in row]))
+    tmp_l.sort()
+    return '|'.join(tmp_l)
 
 
 nums = [-1, 0, 1, 2, -1, -4]
