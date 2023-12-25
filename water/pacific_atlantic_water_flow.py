216f5a7fc5bc0c8c7d10f02e624c08e2e4986375
Tony Su <tonysuglobal@gmail.com>
2023-12-25 11:40:02 -0800

    Pacific Atlantic water flow

---
+++ water/pacific_atlantic_water_flow.py
@@ -0,0 +1,56 @@
+# https://leetcode.com/problems/pacific-atlantic-water-flow/
+
+
+def dfs(row, col, visited, m, last_val=0):
+    if row<0 or col<0 or row>=len(m) or col>=len(m[0]):
+        return
+    if (row, col) in visited:
+        return
+    if m[row][col] < last_val:
+        return
+    visited.add((row, col))
+    dfs(row+1, col, visited, m, m[row][col])
+    dfs(row-1, col, visited, m, m[row][col])
+    dfs(row, col+1, visited, m, m[row][col])
+    dfs(row, col-1, visited, m, m[row][col])
+
+def pacific_atlantic(m):
+    if not m or not m[0]:
+        return []
+    pacific_reachable = set()
+    atlantic_reachable = set()
+
+    for row in range(len(m)):
+        dfs(row, 0, pacific_reachable, m)
+        dfs(row, len(m[0]) - 1, atlantic_reachable, m)
+    for col in range(len(m[0])):
+        dfs(0, col, pacific_reachable, m)
+        dfs(len(m) - 1, col, atlantic_reachable, m)
+
+    result = list(pacific_reachable.intersection(atlantic_reachable))
+    return [list(k) for k in result]
+
+def _to_str(l):
+    for row in l:
+        row.sort()
+        for i in range(len(row)):
+            row[i] = str(row[i])
+    l = [','.join(k) for k in l]
+    l.sort()
+    out = '|'.join(l)
+    return out
+
+heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+expected = [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+assert _to_str(pacific_atlantic(heights)) == _to_str(expected)
+
+
+heights = [[1]]
+expected = [[0,0]]
+assert _to_str(pacific_atlantic(heights)) == _to_str(expected)
+
+
+
+heights = [[1], [1]]
+expected = [[0,0], [1,0]]
+assert _to_str(pacific_atlantic(heights)) == _to_str(expected)
