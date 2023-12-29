1a646dc1aa1e61a6eaf8f63168dfaed581de89a6
Tony Su <tonysuglobal@gmail.com>
2023-12-29 15:30:24 +0100

    Decode ways

---
+++ decode_ways/decode_ways.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/decode-ways/
+
+# '12': dp [1], [1, 2]
+# '226': dp [1], [1, 2, 3]
+
+
+def num_decodings(s):
+    if s[0] == "0":
+        return 0
+    back2 = 1
+    back1 = 1
+    for i in range(1, len(s)):
+        current = 0
+        if s[i] != "0":
+            current = back1
+        two_digit = int(s[i - 1: i + 1])
+        if 10 <= two_digit <= 26:
+            current += back2
+        back2 = back1
+        back1 = current
+    return back1
+
+assert num_decodings('12') == 2
+
+assert num_decodings('226') == 3
+
+assert num_decodings('09') == 0
