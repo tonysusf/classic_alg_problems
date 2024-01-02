c11c77c2d880c72b97318a1654c80569761b6dce
Tony Su <tonysuglobal@gmail.com>
2024-01-02 12:06:46 +0100

    cleanup

--- matrix/set_matrix_zeroes.py
+++ matrix/set_matrix_zeroes.py
@@ -2,7 +2,10 @@
 
 def set_zeroes(matrix):
     print('input', matrix)
+    # use m[0][0] as flag for 1st row; use set_first_col_zero for 1st column
     set_first_col_zero = False
+
+    # scan
     for row in range(len(matrix)):
         if matrix[row][0] == 0:
             set_first_col_zero = True
@@ -11,15 +14,18 @@ def set_zeroes(matrix):
                 matrix[0][col] = 0
                 matrix[row][0] = 0
 
+    # set 0s
     for row in range(1, len(matrix)):
         for col in range(1, len(matrix[0])):
             if matrix[row][0] == 0 or matrix[0][col] == 0:
                 matrix[row][col] = 0
 
+    # m[0][0] for 1st row
     if matrix[0][0] == 0:
         for col in range(len(matrix[0])):
             matrix[0][col] = 0
 
+    # set_first_col_zero for 1st column
     if set_first_col_zero:
         for row in range(len(matrix)):
             matrix[row][0] = 0
@@ -39,3 +45,7 @@ def _to_str(l):
 matrix = [[1,1,1],[1,0,1],[1,1,1]]
 set_zeroes(matrix)
 assert _to_str(matrix) == _to_str([[1, 0, 1],[0, 0, 0],[1, 0, 1]])
+
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+set_zeroes(matrix)
+assert _to_str(matrix) == _to_str([[0,0,0,0],[0,4,5,0],[0,3,1,0]])
--- running_sum/best_time_buy_sell_stock.py
+++ running_sum/best_time_buy_sell_stock.py
@@ -2,11 +2,11 @@
 
 def max_profit(prices):
     cur_min = prices[0]
-    result = 0
+    max_pf = 0
     for i in range(1, len(prices)):
         cur_min = min(cur_min, prices[i-1])
-        result = max(result, prices[i] - cur_min)
-    return result
+        max_pf = max(max_pf, prices[i] - cur_min)
+    return max_pf
 
 prices = [7,1,5,3,6,4]
 assert max_profit(prices) == 5
