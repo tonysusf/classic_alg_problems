7dd9a62b25cac62b2c7fa2036e9e13071afc8a61
Tony Su <tonysuglobal@gmail.com>
2024-02-02 10:08:07 -0800

    improve

--- find_peak_element/find_peak_element_binary_search.py
+++ find_peak_element/find_peak_element_binary_search.py
@@ -3,13 +3,16 @@
 def find_peak_element(nums):
     l = 0
     r = len(nums) - 1
-    while l < r:
-        mid = (l + r) // 2
-        if nums[mid] > nums[mid + 1]: # decreasing, peak should be in left side, so move r to left
-            r = mid
-        else: # increasing, peak should be in right side, so move l to right
-            l = mid + 1
-    return l
+    while l <= r:
+        m = l + (r-l)//2
+        # left neighbour greater, move r towards left side
+        if m > 0 and nums[m-1] > nums[m]:
+            r = m-1
+        # right neighbour greater, move l towards right side
+        elif m < len(nums)-1 and nums[m+1] > nums[m]:
+            l = m+1
+        else: # peak found
+            return m
 
 assert find_peak_element([1,2,3,1]) == 2
 
@@ -17,4 +20,4 @@ assert find_peak_element([1,2,1,3,5,6,4]) == 5
 
 assert find_peak_element([1,2,3]) == 2
 
-assert find_peak_element([1, 3, 1, 2]) == 1
+assert find_peak_element([1,3,1,2]) == 1
