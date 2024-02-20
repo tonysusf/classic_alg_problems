1598048f9cccbdb0974e8c3477f34b23b8858c83
Tony Su <tonysuglobal@gmail.com>
2024-02-20 10:17:03 -0800

    Product of Array Except Self

--- binary_search/find_peak_element.py
+++ binary_search/find_peak_element.py
@@ -3,16 +3,14 @@
 def find_peak_element(nums):
     l = 0
     r = len(nums) - 1
-    while l <= r:
-        m = l + (r-l)//2
-        # left neighbour greater, move r towards left side
-        if m > 0 and nums[m-1] > nums[m]:
-            r = m-1
-        # right neighbour greater, move l towards right side
-        elif m < len(nums)-1 and nums[m+1] > nums[m]:
-            l = m+1
-        else: # peak found
-            return m
+    while l < r:
+        mid = l + (r-l)//2
+        if nums[mid] > nums[mid+1]: # peak in left side or mid
+            r = mid
+        else: # peak in right side
+            l = mid + 1
+    return l
+
 
 assert find_peak_element([1,2,3,1]) == 2
 
--- subarray_product/product_of_array_except_self.py
+++ subarray_product/product_of_array_except_self.py
@@ -2,39 +2,21 @@
 
 
 def product_except_self(nums):
-    n = len(nums)
-    arr1 = [0] * n
-    arr2 = [0] * n
-    result = [0] * n
-    arr1[0] = arr2[n-1] = 1
-    for i in range(1, n):
-        arr1[i] = nums[i-1] * arr1[i-1]
-        arr2[n-i-1] = nums[n-i] * arr2[n-i]
-    
-    for i in range(n):
-        result[i] = arr1[i] * arr2[i]
-    return result
+    l = [0] * len(nums)
+    l[0] = 1
+    r = 1
+    for i in range(1, len(nums)):
+        l[i] = l[i-1] * nums[i-1]
 
-def product_except_self_v2(nums):
-    n = len(nums)
-    result = [0] * n
-    result[0] = 1
-    for i in range(n-1): # skip the last one
-        result[i+1] = nums[i] * result[i]
-
-    right_product = 1
-    for i in range(n): # full scan
-        result[n-i-1] = result[n-i-1] * right_product
-        right_product *= nums[n-i-1]
-
-    return result
+    for i in range(len(nums)-2, -1, -1):
+        r *= nums[i+1]
+        l[i] *= r
+    return l
 
 
 nums = [1,2,3,4]
 assert product_except_self(nums) == [24,12,8,6]
-assert product_except_self_v2(nums) == [24,12,8,6]
 
 nums = [-1,1,0,-3,3]
 assert product_except_self(nums) == [0,0,9,0,0]
-assert product_except_self_v2(nums) == [0,0,9,0,0]
 
