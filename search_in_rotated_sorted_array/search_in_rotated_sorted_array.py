86f099175d4607d7eee2007375fb06164cf10b3e
Tony Su <tonysuglobal@gmail.com>
2026-09-14 14:02:39 -0700

    search_in_rotated_sorted_array

--- search_in_rotated_sorted_array/search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array/search_in_rotated_sorted_array.py
@@ -2,30 +2,29 @@
 # Search in Rotated Sorted Array
 
 def search(nums: list[int], target: int) -> int:
-    print("input is", nums, target)
-    l, r = 0, len(nums) - 1
+    print('input is', nums, 'target is', target)
+    l = 0 #left
+    r = len(nums) - 1 # right
 
     while l <= r:
-        mid = (l + r) // 2
+        p = (l + r) // 2 # pivot or mid
+        print('checking pivot at', p, 'value is', nums[p])
+        if nums[p] == target:
+            print('found index', p)
+            return p
 
-        if nums[mid] == target:
-            print('found it ', mid)
-            return mid
-
-        # when the left side is sorted
-        if nums[l] <= nums[mid]:
-            if nums[l] <= target < nums[mid]:
-                r = mid - 1
+        if nums[l] <= nums[p]:
+            if nums[l] <= target < nums[p]:
+                r = p - 1
             else:
-                l = mid + 1
-
-        # when the right side is sorted
+                l = p + 1
         else:
-            if nums[mid] < target <= nums[r]:
-                l = mid + 1
+            if nums[p] < target <= nums[r]:
+                l = p + 1
             else:
-                r = mid - 1
+                r = p - 1
     return -1
+
 
 assert search([4,5,6,7,0,1,2], 0) == 4
 assert search([4,5,6,7,0,1,2], 3) == -1
