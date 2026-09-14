429bfdb6066abaf08a60fa0b1d55b9d49e4634df
Tony Su <tonysuglobal@gmail.com>
2026-09-14 14:57:51 -0700

    num of islands stack

--- graph_islands/number_of_islands.py
+++ graph_islands/number_of_islands.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/number-of-islands/
 
+from typing import *
+import copy
+
 def num_of_islands(grid):
     count = 0
 
@@ -18,9 +21,33 @@ def num_of_islands(grid):
             if grid[row][col] == '1':
                 count += 1
                 dfs(row, col)
+    print('output is', count)
+    return count
+
+
+def num_of_islands_v2(grid: List[List[str]]) -> int:
+    count = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == '0':
+                continue;
+            count += 1
+            stack = []
+            stack.append((r, c))
+            while stack:
+                curr = stack.pop()
+                for direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    rr = curr[0] + direction[0]
+                    cc = curr[1] + direction[1]
+                    if 0 <= rr < len(grid) and 0 <= cc < len(grid[0]) and grid[rr][cc] == '1':
+                        grid[rr][cc] = '0'
+                        stack.append((rr,cc))
+    print('output is', count)
     return count
 
 
 grid = [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
+grid2 = copy.deepcopy(grid)
 
 assert num_of_islands(grid) == 3
+assert num_of_islands_v2(grid2) == 3
